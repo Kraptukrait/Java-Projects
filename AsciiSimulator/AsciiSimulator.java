6f354d08ddf24e18d37495e48465d2a245c722c8
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-04-22 12:07:53 +0200

    Create AsciiSimulator.java

---
+++ AsciiSimulator/AsciiSimulator.java
@@ -0,0 +1,25 @@
+
+/**
+ *
+ * @author KraptuKrait
+ */
+public class AsciiSimulator {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(" ___       __   _______   ___       ________  ________  _____ ______   _______   ");
+        Thread.sleep(900);
+        System.out.println("|\\  \\     |\\  \\|\\  ___ \\ |\\  \\     |\\   ____\\|\\   __  \\|\\   _ \\  _   \\|\\  ___ \\     ");
+        Thread.sleep(900);
+        System.out.println("\\ \\  \\    \\ \\  \\ \\   __/|\\ \\  \\    \\ \\  \\___|\\ \\  \\|\\  \\ \\  \\\\\\__\\ \\  \\ \\   __/|    ");
+        Thread.sleep(900);
+        System.out.println(" \\ \\  \\  __\\ \\  \\ \\  \\_|/_\\ \\  \\    \\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\|__| \\  \\ \\  \\_|/__  ");
+        Thread.sleep(900);
+        System.out.println("  \\ \\  \\|\\__\\_\\  \\ \\  \\_|\\ \\ \\  \\____\\ \\  \\____\\ \\  \\\\\\  \\ \\  \\    \\ \\  \\ \\  \\_|\\ \\ ");
+        Thread.sleep(900);
+        System.out.println("   \\ \\____________\\ \\_______\\ \\_______\\ \\_______\\ \\_______\\ \\__\\    \\ \\__\\ \\_______\\");
+        Thread.sleep(900);
+        System.out.println("    \\|____________|\\|_______|\\|_______|\\|_______|\\|_______|\\|__|     \\|__|\\|_______|");
+
+    }
+
+}
