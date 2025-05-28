5b05e8cd95c83578bb5af634b529621ffb4f89e2
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-05-28 14:30:37 +0200

    Update Minecraft_Arrays_001.java

--- Arrays/Minecraft_Arrays_001.java
+++ Arrays/Minecraft_Arrays_001.java
@@ -9,9 +9,9 @@ public class Minecraft_Arrays_001 {
         String[] nameInventory = {"Quartz block", "Spawnei", "Oak planks", "Diamond", "Scaffolding", "Raw beef", "Ladder", "Bamboo", "Iron grid"};
 
         //Declare and initialize arrays for the inventory
-        int[] anzahlInventory_S1 = {51, 31, 18, 32, 19, 43, 34, 46, 49};
-        int[] anzahlInventory_S2 = {1, 35, 12, 42, 9, 48, 44, 16, 29};
-        int[] anzahlnventory_S3 = {12, 11, 22, 11, 19, 42, 34, 26, 32};
+        int[] numberInventory_S1 = {51, 31, 18, 32, 19, 43, 34, 46, 49};
+        int[] numberInventory_S2 = {1, 35, 12, 42, 9, 48, 44, 16, 29};
+        int[] numbernventory_S3 = {12, 11, 22, 11, 19, 42, 34, 26, 32};
 
         boolean[] statusInventory_S1 = new boolean[9];
         boolean[] statusInventory_S2 = new boolean[9];
@@ -34,7 +34,7 @@ public class Minecraft_Arrays_001 {
 
         for (int i = 0; i < nameInventory.length; i++) {
             System.out.print("Name: " + nameInventory[i]);
-            System.out.print(" Quantity: " + anzahlInventory_S1[i]);
+            System.out.print(" Quantity: " + numberInventory_S1[i]);
             System.out.print(" Selected: " + statusInventory_S1[i] + "\n");
         }
 
@@ -44,13 +44,13 @@ public class Minecraft_Arrays_001 {
 
         for (int j = 0; j < nameInventory.length; j++) {
             if (nameInventory[j].equalsIgnoreCase(search)) {
-                anzahlInventory_S1[j] = anzahlInventory_S1[j] - 5;
+                numberInventory_S1[j] = numberInventory_S1[j] - 5;
                 statusInventory_S1[j] = true;
             } else {
                 statusInventory_S1[j] = false;
             }
             System.out.print("Name: " + nameInventory[j]);
-            System.out.print(" Quantity: " + anzahlInventory_S1[j]);
+            System.out.print(" Quantity: " + numberInventory_S1[j]);
             System.out.print(" Selected: " + statusInventory_S1[j] + "\n");
         }
 
@@ -60,13 +60,13 @@ public class Minecraft_Arrays_001 {
 
         for (int k = 0; k < nameInventory.length; k++) {
             if (nameInventory[k].equalsIgnoreCase(search)) {
-                anzahlInventory_S1[k] = anzahlInventory_S1[k] - 3;
+                numberInventory_S1[k] = numberInventory_S1[k] - 3;
                 statusInventory_S1[k] = true;
             } else {
                 statusInventory_S1[k] = false;
             }
             System.out.print("Name: " + nameInventory[k]);
-            System.out.print(" Quantity: " + anzahlInventory_S1[k]);
+            System.out.print(" Quantity: " + numberInventory_S1[k]);
             System.out.print(" Selected: " + statusInventory_S1[k] + "\n");
         }
 
@@ -76,13 +76,13 @@ public class Minecraft_Arrays_001 {
 
         for (int a = 0; a < nameInventory.length; a++) {
             if (nameInventory[a].equalsIgnoreCase(search)) {
-                anzahlInventory_S1[a] = anzahlInventory_S1[a] - 20;
+                numberInventory_S1[a] = numberInventory_S1[a] - 20;
                 statusInventory_S1[a] = true;
             } else {
                 statusInventory_S1[a] = false;
             }
             System.out.print("Name: " + nameInventory[a]);
-            System.out.print(" Quantity: " + anzahlInventory_S1[a]);
+            System.out.print(" Quantity: " + numberInventory_S1[a]);
             System.out.print(" Selected: " + statusInventory_S1[a] + "\n");
         }
 
@@ -92,13 +92,13 @@ public class Minecraft_Arrays_001 {
 
         for (int b = 0; b < nameInventory.length; b++) {
             if (nameInventory[b].equalsIgnoreCase(search)) {
-                anzahlInventory_S1[b] = anzahlInventory_S1[b] - 5;
+                numberInventory_S1[b] = numberInventory_S1[b] - 5;
                 statusInventory_S1[b] = true;
             } else {
                 statusInventory_S1[b] = false;
             }
             System.out.print("Name: " + nameInventory[b]);
-            System.out.print(" Quantity: " + anzahlInventory_S1[b]);
+            System.out.print(" Quantity: " + numberInventory_S1[b]);
             System.out.print(" Selected: " + statusInventory_S1[b] + "\n");
         }
     }
