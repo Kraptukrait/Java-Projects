bba909c7fc3aca1debd65659f7c4901bde3f5000
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-05-28 14:33:45 +0200

    Create variable.java

---
+++ Knowledge repository/variable.java
@@ -0,0 +1,35 @@
+
+/**
+ *
+ * @author KraptuKrait
+ */
+public class variable {
+
+    public static void main(String[] args) {
+       //String
+        System.out.println("String");
+        String test = "hello";
+        System.out.println(test);
+
+        //Char
+        System.out.println("Char");
+        char c = 'a';
+        System.out.println(c);
+
+        //Int 
+        System.out.println("int");
+        int a = 5;
+        System.out.println(a);
+
+        // double 
+        System.out.println("double");
+        double d1 = 1.5;
+        System.out.println(d1);
+
+        //boolean 
+        System.out.println("boolean");
+        boolean flag = true;
+        System.out.println(flag);
+
+    }
+}
