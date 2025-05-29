8a35cef42a8645db0c2f03ec05eba6818f976f51
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-05-29 23:37:39 +0200

    Create DoandWhile2.java

---
+++ Knowledge repository/DoandWhile2.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author KraptuKrait
+ */
+public class DoandWhile2 {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter <= 10) {
+            System.out.println(counter);
+            counter++;
+        }
+        System.out.println("Done");
+    }
+}
