92849285d85c3ba8bf7a313b94ab48e0224eefd0
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-06-12 19:35:58 +0200

    Create Main.java

---
+++ GUI/FourFields/Main.java
@@ -0,0 +1,15 @@
+package com.mycompany.FourFieldGUI;
+
+/**
+ *
+ * @author KraptuKrait
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        GUI window = new GUI();
+        window.setLocation(20, 20);
+        window.setSize(300, 400);
+        window.setVisible(true);
+    }
+}
