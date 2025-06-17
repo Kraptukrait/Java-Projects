bee5dc460ea1bc1fd700a5df1e2d7056c3df1bb0
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-06-17 22:00:41 +0200

    Create DisplayBoard.java

---
+++ GUI/SampleDisplayBoard_ActionListener/DisplayBoard.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.SampleDisplayBoard_ActionListener;
+
+/**
+ *
+ * @author KraptuKrait
+ */
+public class DisplayBoard {
+    public static void main(String[] args) throws InterruptedException {
+        TimeoutWindow myWindow = new TimeoutWindow();
+    }
+}
