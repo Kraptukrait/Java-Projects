625323d59f58a4e154034446a18da080d4e5fe90
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-11-15 22:51:41 +0100

    Implement LogSystem class for logging events

---
+++ Log/LogSystem.java
@@ -0,0 +1,44 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+/**
+ * @author Kraptukrait
+ */
+
+public class LogSystem {
+    private String logMessage;
+    private LocalDateTime systemTime;
+    private String systemUser;
+
+    public LogSystem(LocalDateTime time, String user) throws IOException {
+        this.systemTime = time;
+        this.systemUser = user;
+
+        if (systemTime == null || systemUser == null || systemUser.isEmpty()) {
+            writeInLog(LocalDateTime.now() + ",Wrong input!");
+            throw new IllegalArgumentException("Wrong input!");
+        }
+        logMessage = systemTime + "," + systemUser;
+        try{
+        writeInLog(logMessage);
+        }catch(Exception e){
+            logMessage = e.getMessage();
+        }
+    }
+
+    public void writeInLog(String logMessage) throws IOException {
+        this.logMessage = logMessage;
+
+        try (FileWriter file = new FileWriter("protocol.log", true)) {
+            file.write(logMessage + "\n");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String logUser = System.getProperty("user.name");
+        LocalDateTime logTime = LocalDateTime.now();
+
+        LogSystem mSystem = new LogSystem(logTime, logUser);
+    }
+}
